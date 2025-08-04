c61832f4496efea0876f8882ec4b35bc8d3a66d5
uiouio <21changyu@163.com>
2025-08-04 23:03:30 +0800

    Update diy-part1.sh

--- RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part1.sh
+++ RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part1.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
+#===============================================
+# Description: DIY script
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+# Author: uiouio
+# Github: https://github.com/uiouio
+#===============================================
+
+# 修改版本为编译日期，数字类型。
+date_version=$(date +"%Y%m%d%H")
+echo $date_version > version
+
+# 为iStoreOS固件版本加上编译作者
+author="Uiouio"
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V ${date_version} by ${author}'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/OPENWRT_RELEASE.*/OPENWRT_RELEASE=\"%D %V ${date_version} by ${author}\"/g" package/base-files/files/usr/lib/os-release
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#安装helloworld插件
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# Download source codes
+# 拉取我的软件包仓库
+sed -i '$a src-git xmpackage https://github.com/xiaomeng9597/openwrt-packages2.git;main' feeds.conf.default
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
-#安装应用过滤(OAF)插件
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
