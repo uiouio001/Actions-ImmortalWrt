93cf81a5c09b24003002245e97ce286d2749b075
uiouio <21changyu@163.com>
2025-08-09 14:15:30 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -4,9 +4,13 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #===============================================
 
-# 添加feed源地址
-# 常用源
+#==============添加feed源地址===================
+# 常用APP源
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages;master' feeds.conf.default
+# iStoreOS
+sed -i '1i src-git istore https://github.com/linkease/istore;main' feeds.conf.default
+sed -i '1i src-git third_party https://github.com/linkease/istore-packages.git;main' feeds.conf.default
+sed -i '1i src-git linkease_nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
 #Argon Theme
 sed -i '1i src-git argon https://github.com/jerrykuku/luci-theme-argon.git;master' feeds.conf.default
 #OpenClash
