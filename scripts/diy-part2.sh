0ec8d83dfabf3d69a67e24517ebe71e9fe32db71
uiouio <21changyu@163.com>
2025-08-13 23:00:29 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #===============================================
-# File name: diy-part1.sh
+# File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #===============================================
 
