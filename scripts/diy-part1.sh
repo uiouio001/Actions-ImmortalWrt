89b2938f66d2b1489c9facb34942c5780f2ed50a
uiouio001 <uiouio_001@163.com>
2025-08-18 17:54:51 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -10,4 +10,6 @@
 #sed -i '1i src-git argon-config https://github.com/jerrykuku/luci-app-argon-config.git;master' feeds.conf.default
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-
+#灵缇游戏加速器
+git clone https://github.com/esirplayground/LingTiGameAcc.git lingtigameacc
+git clone https://github.com/esirplayground/luci-app-LingTiGameAcc.git package/luci-app-LingTiGameAcc
