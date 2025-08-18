53c5b5a477291f1aee1b19ce5272604ed81b240c
uiouio001 <uiouio_001@163.com>
2025-08-18 23:55:40 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -17,15 +17,22 @@ sed -i "s/OPENWRT_RELEASE.*/OPENWRT_RELEASE=\"%D %V ${date_version} by ${author}
 #sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # 修改默认wifi名称ssid为 旧版本修改
-#sed -i 's/ssid='ImmortalWrt'/ssid='ChinaUnicom-BB4A'/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/ssid='ImmortalWrt'/ssid='ChinaUnicom'/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # 修改默认wifi名称ssid为 hanwckf源码修改 ssid=ImmortalWrt-2.4G/ImmortalWrt-5G
-#sed -i 's/ssid='ImmortalWrt'/ssid='ChinaUnicom-BB4A'/g' ./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+#sed -i 's/ssid='ImmortalWrt'/ssid='ChinaUnicom'/g' ./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+
+# 修改默认wifi密码key为password
+#sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+#取消原主题luci-theme-bootstrap为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
 
-# 修改默认主机名
+# 修改默认主机名（不能纯数字或者使用中文）
 #sed -i 's/OpenWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 # 删除DDNS's示例
