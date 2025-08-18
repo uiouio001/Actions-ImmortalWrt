d47b52f908869aec52f8d60fdbbb14bff8e2d18f
uiouio001 <uiouio_001@163.com>
2025-08-18 17:45:59 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -5,14 +5,9 @@
 #===============================================
 
 #==============添加feed源地址===================
-# 常用APP源
-#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master' feeds.conf.default
-# iStoreOS
-#sed -i '1i src-git istore https://github.com/linkease/istore.git;main' feeds.conf.default
-#sed -i '1i src-git third_party https://github.com/linkease/istore-packages.git;main' feeds.conf.default
-#sed -i '1i src-git linkease_nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
 #Argon Theme
 #sed -i '1i src-git argon https://github.com/jerrykuku/luci-theme-argon.git;master' feeds.conf.default
-#OpenClash
-#sed -i '1i src-git openclash https://github.com/vernesong/OpenClash.git;master' feeds.conf.default
+#sed -i '1i src-git argon-config https://github.com/jerrykuku/luci-app-argon-config.git;master' feeds.conf.default
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
